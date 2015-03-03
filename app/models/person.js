2c26f7bcedbb4b7a16e6e088e874d2d06e196b99
Michael Boselowitz <boselowitz@maya.com>
2015-03-02 23:19:17 -0500

    Add index standings.

--- app/models/person.js
+++ app/models/person.js
@@ -1,5 +1,32 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  
+var Person = DS.Model.extend({
+    name: DS.attr('string'),
+    wins: DS.attr('number'),
+    losses: DS.attr('number')
 });
+
+Person.reopenClass({
+    FIXTURES: [
+        {
+            id: 1,
+            name: 'Michael Boselowitz',
+            wins: 5,
+            losses: 1
+        },
+        {
+            id: 2,
+            name: 'Lori Paul',
+            wins: 1,
+            losses: 3
+        },
+        {
+            id: 3,
+            name: 'Matt Ross',
+            wins: 0,
+            losses: 2
+        }
+    ]
+});
+
+export default Person;
