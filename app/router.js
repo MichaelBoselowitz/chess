7de00337620a3c31bb4b163e77f0a25fd70c8599
Michael Boselowitz <boselowitz@maya.com>
2015-03-02 23:21:17 -0500

    Add individual person page.

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('game', function() {});
   this.resource('person', function() {});
+    this.resource('person', { path: '/person/:person_id' }, function() {});
 });
 
 export default Router;
